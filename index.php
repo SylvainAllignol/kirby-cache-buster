859e0a97b7c5903a5bcde2423472442a3985e9e0
Sylvain Allignol <sylvain@allignol.com>
2026-01-08 18:02:56 +0100

    Initial release

--- index.php
+++ index.php
@@ -2,17 +2,20 @@
 use Kirby\Filesystem\F;
 
 Kirby::plugin('allignol/kirby-cache-buster', [
+	'options' => [
+		'active' => true
+	],
 	'components' => [
 		'css' => function ($kirby, $url) {
 			$file = $kirby->roots()->index() . '/' . $url;
-			if (!$timestamp = F::modified($file)) {
+			if (!$timestamp = F::modified($file) || $kirby->option('allignol.kirby-cache-buster.active') === false) {
 				return $url;
 			}
 			return dirname($url) . '/' . F::name($url) . '.' . hash('xxh3', $timestamp) . '.css';
 		},
 		'js' => function ($kirby, $url) {
 			$file = $kirby->roots()->index() . '/' . $url;
-			if (!$timestamp = F::modified($file)) {
+			if (!$timestamp = F::modified($file) || $kirby->option('allignol.kirby-cache-buster.active') === false) {
 				return $url;
 			}
 			return dirname($url) . '/' . F::name($url) . '.' . hash('xxh3', $timestamp) . '.js';
